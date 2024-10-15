75383eb2278e7bfe62b9586a2e666b8b39087002
PumpkinPeasant <m2314670@edu.misis.ru>
2024-10-15 20:58:17 +0300

    Created rover class

---
+++ classes/rover.ts
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+
+class Rover {
+
+    model: THREE.Group
+
+
+
+    constructor(model: THREE.Group) {
+        this.model = model
+    }
+}
+
+export default Rover
--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { onMounted, ref, onBeforeUnmount } from "vue";
+import {onMounted, ref, onBeforeUnmount} from "vue";
 import Level from '~/classes/level';
 import * as THREE from "three";
+import {loadModel} from "~/composables/useLoader";
+import Rover from "~/classes/rover";
 
 const canvas = ref<HTMLCanvasElement | null>(null);
 let levelInstance: Level | null = null;
-let cube: THREE.Mesh | null = null;
+// let cube: THREE.Mesh | null = null;
 
 const handleResize = () => {
   levelInstance?.resizeRendererToDisplaySize();
@@ -16,28 +18,34 @@ const configureLevel = () => {
     levelInstance = new Level(canvas.value);
     window.addEventListener('resize', handleResize);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial({ color: 0x00aaff });
-    cube = new THREE.Mesh(geometry, material);
-    levelInstance.add(cube);
+    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const material = new THREE.MeshPhongMaterial({ color: 0x00aaff });
+    // cube = new THREE.Mesh(geometry, material);
+    // levelInstance.add(cube);
 
     const light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 20, 10);
     levelInstance.add(light);
-    animateCube();
+
+    loadModel('models/rover.glb').then(model => {
+      const rover = new Rover(model)
+      levelInstance?.add(rover.model)
+    })
+
+    // animateCube();
   }
 };
 
-const animateCube = () => {
-  const animate = () => {
-    if (cube) {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-    }
-    requestAnimationFrame(animate);
-  };
-  animate();
-};
+// const animateCube = () => {
+//   const animate = () => {
+//     if (cube) {
+//       cube.rotation.x += 0.01;
+//       cube.rotation.y += 0.01;
+//     }
+//     requestAnimationFrame(animate);
+//   };
+//   animate();
+// };
 
 onMounted(() => {
   configureLevel();
